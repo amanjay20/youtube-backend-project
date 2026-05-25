e8342caaa23a0d26fac7de4862e15a0aa634291b
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2026-05-25 23:11:51 +0530

    loginUser Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -68,4 +68,67 @@ const registerUser = asyncHandler ( async(req,res)=>{
     )
 })
 
+const generateAccessTokenAndRefreshtoken = async(userId) =>{
+    try {
+        const user  = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave:False})
+        return {accessToken, refreshToken   }
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh token")
+    }
+}
+
+const loginUser = asyncHandler(async(req,res)=>{
+    const { email, username , password } = req.body
+
+    if ( !email && !username ) {
+        throw new ApiError(400,"Email or username is required")
+    }
+
+    const user = await User.findOne({
+        $or:[{username} , {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404,"User Does not exits")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid){
+        throw new ApiError(401, "Invalid User Credentails")
+    }
+
+    const {accessToken,refreshToken} = await generateAccessTokenAndRefreshtoken(user._id)
+
+    const loggedUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken" , accessToken , options)
+    .cookie("refreshToken" ,refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,{
+                user:loggedUser,accessToken,refreshToken
+            },
+            "User Logged In SuccessFully"
+        )
+    )
+
+
+})
+
+
+
+
+
 export {registerUser}
