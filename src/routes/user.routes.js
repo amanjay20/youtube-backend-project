64487ad3c065288c88221369b2ef24b623ef6c42
amanjay20 <83428832+amanjay20@users.noreply.github.com>
2026-06-21 15:18:58 +0530

    style: format user.routes.js for improved readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import { 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,11 +32,28 @@ router.route("/register").post(
     registerUser
 )
 
-router.route("/login").post(loginUser)
+router
+.route("/login")
+.post(loginUser)
 
-router.route("/logout").post( verifyJWT ,logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router
+.route("/logout")
+.post( 
+    verifyJWT ,
+    logoutUser
+)
+
+router
+.route("/refresh-token")
+.post(refreshAccessToken)
+
+router
+.route("/change-password")
+.post(
+    verifyJWT,
+    changeCurrentPassword
+)
+
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 
